38ecdd586cc262c45f54920a949d2375c017a983
vijay <vijaystab@vijays-MacBook-Air.local>
2026-04-17 15:34:36 +0530

    disable eslint errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { createClient } from '@supabase/supabase-js';
